4e282b5f4759d9531aa962046ea7527fb723df6c
yujin1213 <jyoojin1597@naver.com>
2022-05-17 01:34:18 +0900

    로그인페이지 이동, 검색창

--- perfume-website/src/App.js
+++ perfume-website/src/App.js
@@ -1,11 +1,23 @@
 import './App.css';
-import Header from './Components/Header'
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+
+import Header from './Components/Header';
+import Login from "./Components/Login";
+import Main from "./Components/Main";
+import Join from "./Components/Join";
+
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-    </div>
+    <BrowserRouter>
+      <div className="App">
+          <Routes>
+              <Route path="/" element={<Main/>}/>
+              <Route path="/login" element={<Login/>}/>
+              <Route path="/join" element={<Join/>}/>
+          </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
